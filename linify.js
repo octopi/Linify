2e60552f9e8e47c92d4c89ac663029d12ab90ce9
octopi <octopie@gmail.com>
2012-02-16 05:05:33 -0500

    support for hashtag highlighting

--- linify.js
+++ linify.js
@@ -4,7 +4,7 @@ Returns a Jeremy Lin-pun-riddled version of text.
 */
 
 var pairings = [
-	[/Lin/, '<img src="l.png" alt="Lin" />'],
+	[/Lin/, ' <img src="l.png" alt="Lin"/>'],
 	[/ling$/, 'LIN\''],
 	[/lin/i, 'LIN'],
 	[/^in/, 'lin'],
@@ -13,9 +13,12 @@ var pairings = [
 	[/^li/, 'lin'],
 	[/^Li/, 'Lin'],
 ];
+var linified, hashtags; // global scope bc used by two functions
 function linify(text) {
 	var words = text.replace(/\n/g, '<br /> ').split(' ');
-	var linified = '';
+	var imgOffset = 0; // because image tags mess up the word indexing
+	linified = '';
+	hashtags = {}; 
 	for(var i=0;i<words.length;i++) {
 		var curr = words[i];
 		var j = 0;
@@ -24,9 +27,43 @@ function linify(text) {
 			rep = curr.replace(pairings[j][0], pairings[j][1]);
 			j++;
 		} while (rep === curr && j < pairings.length);
-		linified += (rep === '') ? curr : rep;
+
+		// if replaced, check to see if a hashtag exists
+		if(rep !== curr && rep.search(pairings[0][1]) === -1) {
+			console.log('inside rep is '+rep);
+			jQuery.ajax({
+				localI: i,
+				localRep: rep,
+				localImgOffset: imgOffset,
+				url: 'http://search.twitter.com/search.json?q=%23'+rep,
+				dataType: 'jsonp',
+				success: function(data) {
+					if(data.results.length > 0) {
+						console.log(data);
+						hashtags[this.localI+this.localImgOffset] = ' <a href="https://twitter.com/#!/search/realtime/%23'+this.localRep+'">#'+this.localRep+'</a> ';
+						console.log('ht ');
+						console.log(hashtags);
+					}
+				}
+			});
+		}
+		linified += rep;
+		if(rep.search(pairings[0][1]) > -1)
+			imgOffset += 3;
 		if(i<words.length-1)
 			linified += ' ';
 	}
 	return linified;
+}
+
+function hashtagify() {
+	console.log(hashtags);
+	var words = linified.split(' ');
+	for(var i in hashtags) {
+		console.log(i+', '+hashtags[i]);
+		words[i] = hashtags[i];
+	}
+	var joined = words.join(' ');
+	console.log('joined is '+joined);
+	return joined;
 }
